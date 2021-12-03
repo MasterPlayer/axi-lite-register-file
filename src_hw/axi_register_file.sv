2d19b983a20a96b2285223a24542946ac4604a5f
Maxim <masterplayer@yandex.ru>
2021-12-03 18:54:29 +0300

    Update axi_register_file.sv
    fix issues

--- src_hw/axi_register_file.sv
+++ src_hw/axi_register_file.sv
@@ -6,7 +6,8 @@ module axi_register_file #(
     parameter integer DATA_WIDTH = 32          ,
     parameter integer ADDR_WIDTH = 32          ,
     parameter integer REG_COUNT  = 1           ,
-    parameter integer BASEADDR   = 32'h00000000
+    parameter integer BASEADDR   = 32'h00000000,
+    parameter string  ACCESS     = "RW"
 ) (
     input  logic                                      CLK          ,
     input  logic                                      RESETN       ,
@@ -48,18 +49,16 @@ module axi_register_file #(
     logic                  axi_rvalid ;
 
     localparam integer ADDR_LSB_CFG          = (DATA_WIDTH/32) + 1                ;
-    localparam integer EFFECTIVE_ADDR_WIDTH  = $clog2(REG_COUNT)                  ;
-
     localparam integer OPT_MEM_ADDR_BITS_CFG = $clog2(REG_COUNT)                  ;
-    // localparam integer HIGHADDR              = ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG ;
+    localparam integer EFFECTIVE_ADDR_WIDTH  = $clog2(REG_COUNT)                  ;
     localparam integer LOWADDR               = EFFECTIVE_ADDR_WIDTH + ADDR_LSB_CFG;
 
-    logic [(REG_COUNT-1):0][DATA_WIDTH-1:0] register = '{default:'{default:0}};
-    logic [(REG_COUNT-1):0] register_valid = '{default:0};
-    logic                  slv_reg_rden;
-    logic                  slv_reg_wren;
-    logic [DATA_WIDTH-1:0] reg_data_out;
-    logic                  aw_en       ;
+    logic [(REG_COUNT-1):0][DATA_WIDTH-1:0] register       = '{default:'{default:0}};
+    logic [(REG_COUNT-1):0]                 register_valid = '{default:0}           ;
+    logic                                   slv_reg_rden                            ;
+    logic                                   slv_reg_wren                            ;
+    logic [ DATA_WIDTH-1:0]                 reg_data_out                            ;
+    logic                                   aw_en                                   ;
 
     integer byte_index;
 
@@ -74,19 +73,32 @@ module axi_register_file #(
         RVALID  = axi_rvalid;
     end 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    logic wr_addr_flaq = 1'b0;
+    logic rd_addr_flaq = 1'b0;
+
+
+    always_comb begin 
+        if (AWADDR[(ADDR_WIDTH-1):LOWADDR] == BASEADDR[(ADDR_WIDTH-1):LOWADDR]) 
+            wr_addr_flaq = 1'b1;
+        else 
+            wr_addr_flaq = 1'b0;
+    end 
+
+    always_comb begin 
+        if (ARADDR[(ADDR_WIDTH-1):LOWADDR] == BASEADDR[(ADDR_WIDTH-1):LOWADDR]) 
+            rd_addr_flaq = 1'b1;
+        else 
+            rd_addr_flaq = 1'b0;
+    end 
 
     always @( posedge CLK ) begin : axi_awready_proc
         if (~RESETN)
             axi_awready <= 1'b0;
         else    
-            if (~axi_awready && AWVALID && WVALID && aw_en && (AWADDR[(ADDR_WIDTH-1):LOWADDR] == BASEADDR[(ADDR_WIDTH-1):LOWADDR]))
+            if (~axi_awready && AWVALID && WVALID && aw_en && wr_addr_flaq)
                 axi_awready <= 1'b1;
             else 
-                // if (BREADY && axi_bvalid)
-                //     axi_awready <= 1'b0;
-                // else
-                    axi_awready <= 1'b0;
+                axi_awready <= 1'b0;
     end       
 
     always @( posedge CLK ) begin : aw_en_proc
@@ -112,7 +124,7 @@ module axi_register_file #(
         if (~RESETN)
             axi_wready <= 1'b0;
         else    
-            if (~axi_wready && WVALID && AWVALID && aw_en && (AWADDR[(ADDR_WIDTH-1):LOWADDR] == BASEADDR[(ADDR_WIDTH-1):LOWADDR]))
+            if (~axi_wready && WVALID && AWVALID && aw_en && wr_addr_flaq)
                 axi_wready <= 1'b1;
             else
                 axi_wready <= 1'b0;
@@ -145,7 +157,7 @@ module axi_register_file #(
         if (~RESETN)
             axi_arready <= 1'b0;
         else    
-            if (~axi_arready && ARVALID && (ARADDR[ADDR_WIDTH-1:LOWADDR] == BASEADDR[ADDR_WIDTH-1:LOWADDR]))
+            if (~axi_arready && ARVALID && rd_addr_flaq)
                 axi_arready <= 1'b1;
             else
                 axi_arready <= 1'b0;
@@ -194,63 +206,116 @@ module axi_register_file #(
                 axi_rdata <= reg_data_out;     // register read data
     end    
     
-
     generate 
+        if (ACCESS == "RW") begin 
 
-        for (genvar reg_index = 0; reg_index < REG_COUNT; reg_index++) begin 
-    
+            for (genvar reg_index = 0; reg_index < REG_COUNT; reg_index++) begin 
+        
+                always @(posedge CLK) begin : register_proc
+                    if (~RESETN )
+                        register[reg_index] <= 0;
+                    else
+                        if (slv_reg_wren) begin 
+                            if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) begin 
+                                for ( byte_index = 0; byte_index <= (DATA_WIDTH/8)-1; byte_index = byte_index + 1 ) begin 
+                                    if (WSTRB[byte_index] == 1 ) begin 
+                                        // register[reg_index][byte_index] <= WDATA[(byte_index*8) +: 8];
+                                        register[reg_index][(byte_index*8) +: 8] <= WDATA[(byte_index*8) +: 8];
+                                    end 
+                                end 
+                            end 
+                        end else begin 
+                            if (REG_IN_VALID[reg_index]) 
+                                register[reg_index] <= REG_IN[reg_index];
+                        end 
+                end    
+
+                always_ff @(posedge CLK) begin 
+                    if (~RESETN) begin 
+                        register_valid[reg_index] <= 1'b0;
+                    end else begin 
+                        if (slv_reg_wren) begin 
+                            if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) begin 
+                                register_valid[reg_index] <= 1'b1;
+                            end else begin 
+                                register_valid[reg_index] <= 1'b0;                         
+                            end 
+                        end else begin 
+                            register_valid[reg_index] <= 1'b0;                         
+                        end 
+                    end 
+                end 
+
+                always_comb begin
+                    REG_OUT[reg_index]       = register[reg_index];
+                    REG_OUT_VALID[reg_index] = register_valid[reg_index];
+                end 
+
+            end 
+
+        end // END access = RW
+
+
+
+        if (ACCESS == "R") begin 
+
             always @(posedge CLK) begin : register_proc
                 if (~RESETN )
                     register[reg_index] <= 0;
                 else
-                    if (slv_reg_wren) begin 
-                        if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) begin 
-                            for ( byte_index = 0; byte_index <= (DATA_WIDTH/8)-1; byte_index = byte_index + 1 ) begin 
-                                if (WSTRB[byte_index] == 1 ) begin 
-                                    // register[reg_index][byte_index] <= WDATA[(byte_index*8) +: 8];
-                                    register[reg_index][(byte_index*8) +: 8] <= WDATA[(byte_index*8) +: 8];
-                                end 
-                            end 
-                        end 
-                    end else begin 
-                        if (REG_IN_VALID[reg_index]) 
-                            register[reg_index] <= REG_IN[reg_index];
-                    end 
+                    if (REG_IN_VALID[reg_index]) 
+                        register[reg_index] <= REG_IN[reg_index];
             end    
 
-            always_ff @(posedge CLK) begin 
-                if (~RESETN) begin 
-                    register_valid[reg_index] <= 1'b0;
-                end else begin 
-                    if (slv_reg_wren) begin 
-                        if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) begin 
-                            register_valid[reg_index] <= 1'b1;
-                        end else begin 
-                            register_valid[reg_index] <= 1'b0;                         
-                        end 
+        end // end access == R
+
+
+        if (ACCESS == "W") begin 
+
+            for (genvar reg_index = 0; reg_index < REG_COUNT; reg_index++) begin 
+
+                always @(posedge CLK) begin : register_proc
+                    if (~RESETN )
+                        register[reg_index] <= 0;
+                    else
+                        if (slv_reg_wren) 
+                            if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) 
+                                for ( byte_index = 0; byte_index <= (DATA_WIDTH/8)-1; byte_index = byte_index + 1 ) 
+                                    if (WSTRB[byte_index] == 1 ) 
+                                        // register[reg_index][byte_index] <= WDATA[(byte_index*8) +: 8];
+                                        register[reg_index][(byte_index*8) +: 8] <= WDATA[(byte_index*8) +: 8];
+                end    
+
+                always_ff @(posedge CLK) begin 
+                    if (~RESETN) begin 
+                        register_valid[reg_index] <= 1'b0;
                     end else begin 
-                        register_valid[reg_index] <= 1'b0;                         
+                        if (slv_reg_wren) begin 
+                            if (axi_awaddr[ADDR_LSB_CFG+OPT_MEM_ADDR_BITS_CFG:ADDR_LSB_CFG] == reg_index) begin 
+                                if (WSTRB) begin 
+                                    register_valid[reg_index] <= 1'b1;
+                                end else begin 
+                                    register_valid[reg_index] <= 1'b0;  
+                                end   
+                            end else begin 
+                                register_valid[reg_index] <= 1'b0;
+                            end 
+                        end else begin 
+                            register_valid[reg_index] <= 1'b0;
+                        end 
                     end 
                 end 
+
+                always_comb begin
+                    REG_OUT[reg_index]       = register[reg_index];
+                    REG_OUT_VALID[reg_index] = register_valid[reg_index];
+                end 
+
             end 
 
-            always_comb begin
-                REG_OUT[reg_index]       = register[reg_index];
-                REG_OUT_VALID[reg_index] = register_valid[reg_index];
-            end 
+        end // end access = write
 
-        end 
 
     endgenerate
-    // generate
-    //     for (genvar index = 0; index < REG_COUNT; index++) begin 
-    //         always_comb begin 
-    //             register[index] = REG_IN[index];
-    //         end         
-    //     end 
-    // endgenerate
-
-
-
 
 endmodule
